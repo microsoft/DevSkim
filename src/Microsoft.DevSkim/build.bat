16ae1351b72b7bf1d0be048f789fca61b3ce18dc
Pavel Bansky <pabans@microsoft.com>
2018-09-26 12:47:25 -0700

    Updates for zipslip

--- src/Microsoft.DevSkim/build.bat
+++ src/Microsoft.DevSkim/build.bat
@@ -21,6 +21,8 @@ dotnet publish Microsoft.DevSkim.CLI --configuration Release --framework netcore
 dotnet publish Microsoft.DevSkim.CLI --configuration Release --framework netcoreapp2.0 --runtime linux-x64
 dotnet publish Microsoft.DevSkim.CLI --configuration Release --framework netcoreapp2.0 --runtime osx-x64
 
+d:\nuget pack Microsoft.DevSkim.CLI\Microsoft.DevSkim.CLI.nuspec -OutputDirectory Microsoft.DevSkim.CLI\bin\Release\netcoreapp2.0
+
 rem echo CREATING TEMP DIRECTORY FOR .DEB PACKAGE
 rem mkdir temp\devskim-ver_amd64
 rem xcopy Microsoft.DevSkim.CLI\Packaging\LinuxDeb\*.* temp\devskim-ver_amd64 /E
