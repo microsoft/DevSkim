5a6f58264061159e7d35f0f3a78bc62dea12a109
David Alcantar <daalcant@microsoft.com>
2020-02-11 14:17:53 -0800

    Update targets, fix AssemblyInfo error in VSExt.Tests

--- DevSkim-DotNet/Microsoft.DevSkim.VSExtension.Tests/Properties/AssemblyInfo.cs
+++ DevSkim-DotNet/Microsoft.DevSkim.VSExtension.Tests/Properties/AssemblyInfo.cs
@@ -14,7 +14,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("2e699f46-e217-4fb7-b375-c0522759be82")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
