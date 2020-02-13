79568d7a27acef47163eb35f1526281dcd8ea787
Gabe Stocco <gstocco@microsoft.com>
2020-02-13 00:28:07 -0800

    Add new packing scripts.

---
+++ DevSkim-DotNet/Microsoft.DevSkim.CLI/pack_cli_rules.ps1
@@ -0,0 +1,11 @@
+$out = @()
+
+$jsonFiles = Get-ChildItem -Path $path -Filter *.json -Recurse
+ForEach-Object -InputObject $jsonFiles
+{
+    $out = @(out; Get-Content -Path $_.FullName -Raw | ConvertFrom-Json)
+}
+
+$out |
+    ConvertTo-Json -Depth 5 |
+    Out-File -FilePath Microsoft.DevSkim.Cli/Resources/devskim-rules.json
