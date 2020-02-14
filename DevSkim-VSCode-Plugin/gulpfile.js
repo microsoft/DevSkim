a559f4b7a68be122172bc1da189afe3ebc85162b
Gabe Stocco <gstocco@microsoft.com>
2020-02-14 10:51:39 -0800

    Fix gulpfile

--- DevSkim-VSCode-Plugin/gulpfile.js
+++ DevSkim-VSCode-Plugin/gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp');
 var nbgv = require('nerdbank-gitversioning')
-
 const fs = require('fs');
 const util = require('util');
+const renameAsync = util.promisify(fs.rename);
+const { exec } = require('child_process');
 
-gulp.task('setPackageVersion', function() {
-    const renameAsync = util.promisify(fs.rename);
-
-    return renameAsync('package-lock.json', 'package-lock.backup').then(function() {
-        return nbgv.setPackageVersion().finally(function() {
-            return renameAsync('package-lock.backup', 'package-lock.json');
-        });
-    });
+gulp.task('setPackageVersion', function(done) {
+    const gitVersion = nbgv.getVersion();
+    exec(`npm version ${gitVersion.npmPackageVersion} --no-git-tag-version --allow-same-version`);
+    done();
 });
+
+gulp.task('resetPackageVersion', function(done){
+    exec(`npm version 0.0.0-placeholder --no-git-tag-version --allow-same-version`);
+    done();
+});
+
+gulp.task('default', gulp.series('setPackageVersion'));
