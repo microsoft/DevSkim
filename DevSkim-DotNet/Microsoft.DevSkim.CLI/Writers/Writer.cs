d751d401ec0ed370609fbd3eeb2a25a06156322e
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-07-14 22:22:05 -0700

    Update to Sarif 2 SDK

--- DevSkim-DotNet/Microsoft.DevSkim.CLI/Writers/Writer.cs
+++ DevSkim-DotNet/Microsoft.DevSkim.CLI/Writers/Writer.cs
@@ -1,18 +1,24 @@
 ﻿// Copyright (C) Microsoft. All rights reserved. Licensed under the MIT License.
 
+using System;
 using System.IO;
 
 namespace Microsoft.DevSkim.CLI.Writers
 {
-    public abstract class Writer
+    public abstract class Writer : IDisposable
     {
 #nullable disable
 
-        public TextWriter TextWriter { get; set; }
+        public StreamWriter TextWriter { get; set; }
 #nullable restore
 
         public abstract void FlushAndClose();
 
         public abstract void WriteIssue(IssueRecord issue);
+
+        public void Dispose()
+        {
+            TextWriter.Dispose();
+        }
     }
 }
