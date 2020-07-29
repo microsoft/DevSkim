8e70ccd22eb15a7a22ad6f40c9e14640cf5a1e3a
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-07-28 18:10:37 -0700

    Fix Results Serialization

--- DevSkim-DotNet/Microsoft.DevSkim.Blazor/Results.cs
+++ DevSkim-DotNet/Microsoft.DevSkim.Blazor/Results.cs
@@ -7,10 +7,22 @@ namespace Microsoft.DevSkim.Blazor
 {
     public class Results
     {
+        public Results() {
+            ResultLocations = new Dictionary<string, string>();
+            RunIds = new List<string>();
+            FileLocations = new Dictionary<string, Dictionary<string, string>>();
+        }
+
+        public Results(Dictionary<string, string> ResultLocations, List<string> RunIds, Dictionary<string, Dictionary<string, string>> FileLocations)
+        {
+            this.ResultLocations = ResultLocations;
+            this.RunIds = RunIds;
+            this.FileLocations = FileLocations;
+        }
         // Maps RunId to Location
-        public Dictionary<string, string> ResultLocations = new Dictionary<string, string>();
-        public List<string> RunIds = new List<string>();
+        public Dictionary<string, string> ResultLocations { get; set; }
+        public List<string> RunIds { get; set; }
         // Maps RunId to Dictionary of FileName to Location
-        public Dictionary<string, Dictionary<string, string>> FileLocations = new Dictionary<string, Dictionary<string, string>>();
+        public Dictionary<string, Dictionary<string, string>> FileLocations { get; set; }
     }
 }
