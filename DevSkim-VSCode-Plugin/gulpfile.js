cde148549c561309109b5087e22e566430a78b42
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-01-28 15:22:55 -0800

    Remove gulp dependency.

--- DevSkim-VSCode-Plugin/gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var nbgv = require('nerdbank-gitversioning');
-
-gulp.task('setPackageVersion', () => nbgv.setPackageVersion());
-
-gulp.task('resetPackageVersion', () => nbgv.resetPackageVersionPlaceholder());
