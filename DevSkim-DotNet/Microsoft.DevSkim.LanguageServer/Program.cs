9bdfae3d66364792ab099883108675e1a2129c1f
David Alcantar <50344095+daalcant@users.noreply.github.com>
2023-02-13 16:38:42 -0800

    Log only in debug builds

--- DevSkim-DotNet/Microsoft.DevSkim.LanguageServer/Program.cs
+++ DevSkim-DotNet/Microsoft.DevSkim.LanguageServer/Program.cs
@@ -18,22 +18,25 @@ internal class Program
 
     private static async Task MainAsync(string[] args)
     {
-        // Debugger.Launch();
-        // while (!Debugger.IsAttached)
-        // {
-        //     await Task.Delay(100);
-        // }
+    #if DEBUG
+        Debugger.Launch();
+        while (!Debugger.IsAttached)
+        {
+            await Task.Delay(100);
+        }
 
         Log.Logger = new LoggerConfiguration()
-                    .Enrich.FromLogContext()
-                    .WriteTo.File("devskim-server-log.txt", rollingInterval: RollingInterval.Day)
-                    .MinimumLevel.Verbose()
-                    .CreateLogger();
+            .Enrich.FromLogContext()
+            .WriteTo.File("devskim-server-log.txt", rollingInterval: RollingInterval.Day)
+            .MinimumLevel.Verbose()
+            .CreateLogger();
+    #else
+        // Creates a "silent" logger
+        Log.Logger = new LoggerConfiguration().CreateLogger();
+    #endif
 
         Log.Logger.Debug("Configuring server...");
-
         IObserver<WorkDoneProgressReport> workDone = null!;
-
         var server = await OmniSharp.Extensions.LanguageServer.Server.LanguageServer.From(
             options =>
                 options
