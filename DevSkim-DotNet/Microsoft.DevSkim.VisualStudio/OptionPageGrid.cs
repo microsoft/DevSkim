ff3943f6c6ce713e8ca64c4bffec5c38bae666b8
Gabe Stocco <98900+gfs@users.noreply.github.com>
2023-03-27 12:05:26 -0700

    Add a few options to the options pane

---
+++ DevSkim-DotNet/Microsoft.DevSkim.VisualStudio/OptionPageGrid.cs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.DevSkim.VisualStudio
+{
+    using Microsoft.VisualStudio.Shell;
+    using System.ComponentModel;
+
+    public class OptionPageGrid : DialogPage
+    {
+        [Category("Scan Options")]
+        [DisplayName("Scan On Save")]
+        [Description("Scan When Document Is Saved")]
+        public bool ScanOnSave
+        {
+            get; set;
+        } = true;
+        [Category("Scan Options")]
+        [DisplayName("Scan On Change")]
+        [Description("Scan When Document Is Changed")]
+        public bool ScanOnChange
+        {
+            get; set;
+        } = true;
+        [Category("Scan Options")]
+        [DisplayName("Scan On Open")]
+        [Description("Scan When Document Is Opened")]
+        public bool ScanOnOpen
+        {
+            get; set;
+        } = true;
+
+
+    }
+}
---
+++ DevSkim-DotNet/Microsoft.DevSkim.VisualStudio/RuleOptionPageGrid.cs
@@ -0,0 +1,17 @@
+﻿namespace Microsoft.DevSkim.VisualStudio
+{
+    using Microsoft.VisualStudio.Shell;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+
+    public class RuleOptionPageGrid : DialogPage
+    {
+        [Category("Rule Options")]
+        [DisplayName("Custom Rule Paths")]
+        [Description("Paths to load cutom rules from")]
+        public List<string> CustomRulePaths
+        {
+            get; set;
+        } = new List<string>();
+    }
+}
