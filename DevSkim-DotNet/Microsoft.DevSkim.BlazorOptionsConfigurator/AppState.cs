6cd008bde61881cbbc718a32d47fa4ffdc53ca1c
Gabe Stocco <98900+gfs@users.noreply.github.com>
2023-06-26 16:37:07 -0700

    WIP

---
+++ DevSkim-DotNet/Microsoft.DevSkim.BlazorOptionsConfigurator/AppState.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.DevSkim.BlazorOptionsConfigurator
+{
+    using Microsoft.DevSkim.CLI.Options;
+    public static class AppState
+    {
+        public static SerializedAnalyzeCommandOptions commandOptions = new SerializedAnalyzeCommandOptions();
+    }
+}
---
+++ DevSkim-DotNet/Microsoft.DevSkim.BlazorOptionsConfigurator/Client/Program.cs
@@ -0,0 +1,20 @@
+namespace Microsoft.DevSkim.BlazorOptionsConfigurator
+{
+    using Microsoft.AspNetCore.Components.Web;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.DevSkim.BlazorOptionsConfigurator;
+    
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
