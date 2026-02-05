4572df24bf983f201dcec56de1565752ff8f48cf
Giulia Stocco <gstocco@microsoft.com>
2026-02-05 14:42:31 -0800

    Clean up language server process

--- DevSkim-DotNet/Microsoft.DevSkim.VisualStudio/DevSkimLanguageServerProvider.cs
+++ DevSkim-DotNet/Microsoft.DevSkim.VisualStudio/DevSkimLanguageServerProvider.cs
@@ -3,13 +3,16 @@
 
 namespace Microsoft.DevSkim.VisualStudio;
 
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO.Pipelines;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using global::Microsoft.VisualStudio.Extensibility;
 using global::Microsoft.VisualStudio.Extensibility.Editor;
 using global::Microsoft.VisualStudio.Extensibility.LanguageServer;
 using global::Microsoft.VisualStudio.RpcContracts.LanguageServerProvider;
+using Microsoft.Win32.SafeHandles;
 using Nerdbank.Streams;
 
 /// <summary>
@@ -21,6 +24,7 @@ using Nerdbank.Streams;
 internal class DevSkimLanguageServerProvider : LanguageServerProvider
 {
     private Process? _serverProcess;
+    private SafeHandle? _jobHandle;
 
     /// <inheritdoc/>
     public override LanguageServerProviderConfiguration LanguageServerProviderConfiguration => new(
@@ -47,6 +51,9 @@ internal class DevSkimLanguageServerProvider : LanguageServerProvider
             return Task.FromResult<IDuplexPipe?>(null);
         }
 
+        // Create a Windows Job Object so the server process is killed when the host process exits
+        EnsureJobObject();
+
         var startInfo = new ProcessStartInfo
         {
             FileName = serverPath,
@@ -71,6 +78,7 @@ internal class DevSkimLanguageServerProvider : LanguageServerProvider
         if (process.Start())
         {
             _serverProcess = process;
+            AssignProcessToJobObject(process);
             process.BeginErrorReadLine();
             File.AppendAllText(logPath, $"[{DateTime.Now}] Language server started (PID: {process.Id})\n");
 
@@ -110,6 +118,8 @@ internal class DevSkimLanguageServerProvider : LanguageServerProvider
         if (isDisposing)
         {
             StopServerProcess();
+            _jobHandle?.Dispose();
+            _jobHandle = null;
         }
 
         base.Dispose(isDisposing);
@@ -135,6 +145,59 @@ internal class DevSkimLanguageServerProvider : LanguageServerProvider
         }
     }
 
+    private void EnsureJobObject()
+    {
+        if (_jobHandle != null)
+        {
+            return;
+        }
+
+        try
+        {
+            string jobName = "DevSkimLanguageServer" + Environment.ProcessId;
+            _jobHandle = NativeMethods.CreateJobObject(IntPtr.Zero, jobName);
+
+            var extendedInfo = new NativeMethods.JOBOBJECT_EXTENDED_LIMIT_INFORMATION
+            {
+                BasicLimitInformation = new NativeMethods.JOBOBJECT_BASIC_LIMIT_INFORMATION
+                {
+                    LimitFlags = 0x2000 /* JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE */
+                }
+            };
+
+            int length = Marshal.SizeOf(extendedInfo);
+            IntPtr pExtendedInfo = Marshal.AllocHGlobal(length);
+            try
+            {
+                Marshal.StructureToPtr(extendedInfo, pExtendedInfo, false);
+                NativeMethods.SetInformationJobObject(_jobHandle, 9 /* JobObjectExtendedLimitInformation */, pExtendedInfo, (uint)length);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(pExtendedInfo);
+            }
+        }
+        catch
+        {
+            // Non-fatal: process cleanup will fall back to Dispose/Kill
+        }
+    }
+
+    private void AssignProcessToJobObject(Process process)
+    {
+        try
+        {
+            if (_jobHandle != null && !_jobHandle.IsInvalid)
+            {
+                NativeMethods.AssignProcessToJobObject(_jobHandle, process.Handle);
+            }
+        }
+        catch
+        {
+            // Non-fatal
+        }
+    }
+
     private static string GetLanguageServerPath()
     {
         var extensionDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -144,6 +207,54 @@ internal class DevSkimLanguageServerProvider : LanguageServerProvider
         }
 
         return Path.Combine(extensionDirectory, "Server", "Microsoft.DevSkim.LanguageServer.exe");
+    }
+
+    private static class NativeMethods
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern SafeWaitHandle CreateJobObject(IntPtr lpJobAttributes, string? lpName);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool SetInformationJobObject(SafeHandle hJob, int jobObjectInfoClass, IntPtr lpJobObjectInfo, uint cbJobObjectInfoLength);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool AssignProcessToJobObject(SafeHandle hJob, IntPtr hProcess);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct JOBOBJECT_BASIC_LIMIT_INFORMATION
+        {
+            public long PerProcessUserTimeLimit;
+            public long PerJobUserTimeLimit;
+            public uint LimitFlags;
+            public UIntPtr MinimumWorkingSetSize;
+            public UIntPtr MaximumWorkingSetSize;
+            public uint ActiveProcessLimit;
+            public UIntPtr Affinity;
+            public uint PriorityClass;
+            public uint SchedulingClass;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct IO_COUNTERS
+        {
+            public ulong ReadOperationCount;
+            public ulong WriteOperationCount;
+            public ulong OtherOperationCount;
+            public ulong ReadTransferCount;
+            public ulong WriteTransferCount;
+            public ulong OtherTransferCount;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct JOBOBJECT_EXTENDED_LIMIT_INFORMATION
+        {
+            public JOBOBJECT_BASIC_LIMIT_INFORMATION BasicLimitInformation;
+            public IO_COUNTERS IoInfo;
+            public UIntPtr ProcessMemoryLimit;
+            public UIntPtr JobMemoryLimit;
+            public UIntPtr PeakProcessMemoryUsed;
+            public UIntPtr PeakJobMemoryUsed;
+        }
     }
 }
 #pragma warning restore VSEXTPREVIEW_LSP
